0683fc744f4e3a80932a7775c9e931e8ab53e375
proxima19 <44165934+proxima19a@users.noreply.github.com>
2024-01-30 16:57:18 +0300

    Add files via upload
    Task #2 added

--- task2.js
+++ task2.js
@@ -1,1 +1,20 @@
 // 2
+let clientName='Игорь';
+let clientSpentForAllTime=110;
+let clientSpentToday=25;
+let discount=0;
+
+if (clientSpentForAllTime>=100 && clientSpentForAllTime<300){
+  discount=10;
+  alert(`Вам предоставляется скидка в ${discount}%!`);
+}else if (clientSpentForAllTime>=300 && clientSpentForAllTime<500){
+  discount=20;
+  alert(`Вам предоставляется скидка в ${discount}%!`);
+}else if (clientSpentForAllTime>=500){
+  discount=30;
+  alert(`Вам предоставляется скидка в ${discount}%!`);
+}
+
+clientSpentToday=clientSpentToday - (clientSpentToday * discount) / 100;
+clientSpentForAllTime+=clientSpentToday;
+alert(`Спасибо, ${clientName}! К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}$.`);
