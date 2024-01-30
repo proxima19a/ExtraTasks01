60cd12703a5dec01f042497be7926ebc211108c6
proxima19 <44165934+proxima19a@users.noreply.github.com>
2024-01-30 17:05:33 +0300

    Add files via upload
    Task #3 added

--- task3.js
+++ task3.js
@@ -1,1 +1,27 @@
 // 3
+let clientName=prompt('Введите имя клиента');
+let clientSpentToday=prompt("Сколько клиент потратил сегодня?");
+let clientSpentForAllTime=prompt("Сколько клиент потратил за все время?");
+clientSpentForAllTime=Number(clientSpentForAllTime);
+clientSpentToday = Number(clientSpentToday);
+let discount=0;
+
+if ( !clientSpentForAllTime || !clientSpentToday ){
+ alert('Сумма, которую клиент потратил за все время и которую потратил сегодня, должна быть числом! Перезагрузи страницу, чтобы повторить попытку.')
+}else {
+
+    if (clientSpentForAllTime>=100 && clientSpentForAllTime<300){
+    discount=10;
+    alert(`Вам предоставляется скидка в ${discount}%!`);
+    }else if (clientSpentForAllTime>=300 && clientSpentForAllTime<500){
+    discount=20;
+    alert(`Вам предоставляется скидка в ${discount}%!`);
+    }else if (clientSpentForAllTime>=500){
+    discount=30;
+    alert(`Вам предоставляется скидка в ${discount}%!`);
+    }
+
+    clientSpentToday=clientSpentToday - (clientSpentToday * discount) / 100;
+    clientSpentForAllTime+=clientSpentToday;
+    alert(`Спасибо, ${clientName}! К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}$.`)
+}
